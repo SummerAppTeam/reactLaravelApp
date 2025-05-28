4fb7de4a311090c4b21b069edf382446b69238c9
Huttunenbe <beniittahuttunen@gmail.com>
2025-05-28 19:58:47 +0300

    clean backend file, add pages, components, hooks folders and files, define routes, css for header and footer, add mobile menu

---
+++ frontend/src/Pages/About/About.jsx
@@ -0,0 +1,14 @@
+
+
+
+const About =() => {
+return ( 
+ <div><main>
+    <h2>About</h2>
+    </main>
+ </div>   
+)
+}
+
+
+export default About;
---
+++ frontend/src/Pages/AddEvent/AddEvent.jsx
@@ -0,0 +1,14 @@
+
+
+
+const AddEvent =() => {
+return ( 
+ <div><main>
+    <h2>Add event</h2>
+    </main>
+ </div>   
+)
+}
+
+
+export default AddEvent;

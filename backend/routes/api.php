67ecb659e56b7b25c7482f98aca1e38c6bd4cc34
Huttunenbe <beniittahuttunen@gmail.com>
2025-06-02 22:49:04 +0300

    add comments

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -3,7 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\EventController;
 
-Route::apiResource('events', EventController::class);
+Route::apiResource('events', EventController::class); // should work on its own
 Route::get('/events', [EventController::class, 'index'])->name('event.index'); 
 Route::post('/store', [EventController::class, 'store']);
 Route::get('/show/{id}', [EventController::class, 'show']);

f4fce65d1fb4fa649a2b9e7b68e1167381d2e7b0
Huttunenbe <beniittahuttunen@gmail.com>
2025-06-02 11:09:35 +0300

    add dark and light mode toggle, emojis changing on event type and event tickets

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -2,9 +2,11 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
+use App\Http\Controllers\API\EventController;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
 Route::middleware('auth:sanctum')->post('/theme', [UserController::class, 'updateTheme']);
+// Route::get('/events', [EventController::class, 'events'])->name('events');

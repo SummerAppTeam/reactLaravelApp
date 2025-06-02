5c50900ca98bdd4a0d9fee33c189c3024eef9b2f
Huttunenbe <beniittahuttunen@gmail.com>
2025-06-02 22:49:04 +0300

    add comments

--- backend/config/cors.php
+++ backend/config/cors.php
@@ -1,10 +1,10 @@
 <?php
 
 return [
-    'paths' => ['api/*', 'events', 'store', 'update/*', 'delete/*'],
+    'paths' => ['api/*', 'events', 'store', 'update/*', 'delete/*'], // I changed this too. Paths. * = matches anything that starts with __/*! 
     'allowed_methods' => ['*'],
     'allowed_origins' => [
-    'http://localhost:5179'
+    'http://localhost:5180'
 ],
 //From online. This is only thing I changed.
     'allowed_origins_patterns' => [], 

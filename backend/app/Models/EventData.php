1beb13f9f8ae1af1d8696cdee27144e5e0cabf6d
soniaakter08 <asonia3308@gmail.com>
2025-06-04 10:30:43 +0300

    add with mapview and addEventForm

--- backend/app/Models/EventData.php
+++ backend/app/Models/EventData.php
@@ -1,20 +1,47 @@
 <?php
 
+// namespace App\Models;
+
+// use Illuminate\Database\Eloquent\Model;
+
+// class EventData extends Model
+// {
+
+//     protected $fillable = [
+//         'name',
+//         'description',
+//         'date',
+//         'location',
+//         'type',
+//         'price',
+//         'image',
+//         'long_description'
+//     ];
+// }
+
+
+
+
+
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class EventData extends Model
 {
+    use HasFactory;
+
+    protected $table = 'event_data';
 
     protected $fillable = [
         'name',
         'description',
+        'long_description',
         'date',
         'location',
         'type',
         'price',
         'image',
-        'long_description'
     ];
 }
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,23 +1,22 @@
+
 import "./App.css";
-import { BrowserRouter, Routes, Route } from "react-router";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import EventContainer from "./Pages/EventList/EventContainer";
 import ScrollToTop from "react-scroll-to-top";
 import FrontPage from "./Pages/FrontPage/Frontpage";
 import About from "./Pages/About/About";
-import EventList from "./Pages/EventList/EventList";
 import AddEvent from "./Pages/AddEvent/AddEvent";
 import DarkModeToggle from "./Components/Header/darkModeToggle";
 import Header from "./Components/Header/Header";
 import Footer from "./Components/Footer/Footer";
 import EventDetails from "./Pages/EventList/EventDetails";
-import "./App.css";
+import EditEvent from "./Pages/EventList/EditEvent";
 
 function App() {
   return (
     <BrowserRouter>
       <div className="App">
-        <DarkModeToggle />{" "}
-        {/*Remember to install DarkModeSwitch from react for this to work*/}
+        <DarkModeToggle />
         <Header />
         <main>
           <Routes>
@@ -26,12 +25,9 @@ function App() {
             <Route path="/EventList" element={<EventContainer />} />
             <Route path="/AddEvent" element={<AddEvent />} />
             <Route path="/events/:id" element={<EventDetails />} />
+            <Route path="/events/:id/edit" element={<EditEvent />} />
           </Routes>
-
-          <div>
-            <ScrollToTop smooth />{" "}
-            {/*Remember to install scrolltotop from react for this to work*/}
-          </div>
+          <ScrollToTop smooth />
         </main>
       </div>
       <Footer />
@@ -40,3 +36,65 @@ function App() {
 }
 
 export default App;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import "./App.css";
+// import { BrowserRouter, Routes, Route } from "react-router";
+// import EventContainer from "./Pages/EventList/EventContainer";
+// import ScrollToTop from "react-scroll-to-top";
+// import FrontPage from "./Pages/FrontPage/Frontpage";
+// import About from "./Pages/About/About";
+// import EventList from "./Pages/EventList/EventList";
+// import AddEvent from "./Pages/AddEvent/AddEvent";
+// import DarkModeToggle from "./Components/Header/darkModeToggle";
+// import Header from "./Components/Header/Header";
+// import Footer from "./Components/Footer/Footer";
+// import EventDetails from "./Pages/EventList/EventDetails";
+// import "./App.css";
+
+// function App() {
+//   return (
+//     <BrowserRouter>
+//       <div className="App">
+//         <DarkModeToggle />{" "}
+//         {/*Remember to install DarkModeSwitch from react for this to work*/}
+//         <Header />
+//         <main>
+//           <Routes>
+//             <Route path="/" element={<FrontPage />} />
+//             <Route path="/about" element={<About />} />
+//             <Route path="/EventList" element={<EventContainer />} />
+//             <Route path="/AddEvent" element={<AddEvent />} />
+//             <Route path="/events/:id" element={<EventDetails />} />
+//           </Routes>
+
+//           <div>
+//             <ScrollToTop smooth />{" "}
+//             {/*Remember to install scrolltotop from react for this to work*/}
+//           </div>
+//         </main>
+//       </div>
+//       <Footer />
+//     </BrowserRouter>
+//   );
+// }
+
+// export default App;

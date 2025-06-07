b5836b15e7fcc25934a79d1f40c433072f873429
Huttunenbe <beniittahuttunen@gmail.com>
2025-06-07 17:23:25 +0300

    add css for footer contact section

--- frontend/src/Components/Footer/Footer.jsx
+++ frontend/src/Components/Footer/Footer.jsx
@@ -21,20 +21,18 @@ function Footer(props) {
           </li>
         </ul>
       </nav>
-<section className="contact">
-  <h4>We’d Love to Hear From You!</h4>
-  <p>
-    Have questions or just want to say hi? <br />
-    Feel free to reach out anytime at <br />
-    contact@events.com 📧
-  </p>
-  <p>
-    Or give us a call at <br />
-    +044 567 890 ☎️ <br />
-    We’re always happy to help!
-  </p>
-</section>
+
       <Newsletter />
+
+      <section className="contact">
+        <p>
+          Reach us anytime at<br></br>
+          contact@events.com <br></br>
+          Or call us at <br />
+          +044 567 890 ☎️ <br />
+          We're always happy to help!
+        </p>
+      </section>
       <h3>Copyright {props.className}</h3>
     </footer>
   );
